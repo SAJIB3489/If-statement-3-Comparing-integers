3a4f6c97ab28d3e3374698f19665731247136447
Md Sajib Pramanic <118989261+SAJIB3489@users.noreply.github.com>
2023-03-12 16:38:12 +0200

    Add files via upload

---
+++ Comparing integers.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>  
+#include<math.h>
+
+int main(int  argc, char** argv) {
+
+	int one, two;
+	printf("Enter two number:\n");
+	scanf("%d %d", &one, &two);
+	if (one > two) {
+		printf("%d is largest\n", one);
+	}
+	else if (two > one) {
+		printf("%d is largest\n", two);
+	}
+}
